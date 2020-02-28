116736e341d8b037be91b91716ed0b0b6f7b1043
Priscille Daoulas <priscilledaoulas@gmail.com>
2020-02-28 21:39:01 +0100

    Ajout tests

--- pglp_3_2/src/test/java/Priscille_DAOULAS/pglp_3_2/EmployeTest.java
+++ pglp_3_2/src/test/java/Priscille_DAOULAS/pglp_3_2/EmployeTest.java
@@ -1,7 +1,16 @@
 package Priscille_DAOULAS.pglp_3_2;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class EmployeTest {
+	
+	@Test
+	public void testSalaire() throws Exception {
+
+		Employe e = new Employe(2005);
+		assertTrue(e.salaire() == 1500 + (2020 - 2005) * 20);
+	}
 
 }
--- pglp_3_2/src/test/java/Priscille_DAOULAS/pglp_3_2/ManagerTest.java
+++ pglp_3_2/src/test/java/Priscille_DAOULAS/pglp_3_2/ManagerTest.java
@@ -1,7 +1,15 @@
 package Priscille_DAOULAS.pglp_3_2;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class ManagerTest {
+	@Test
+	public void testManager() {
 
+		Manager m = new Manager(10);
+		assertTrue(m.salaire() == 1500 + 1000);
+
+	}
 }
